3016291b93e3c388cdae1ff1652a41158c0eaf54
Wolf2323 <gabrielpatrikurban@gmail.com>
2026-06-03 09:30:06 +0200

    Remove `fast-content-type-parse` package and update dependencies: `content-type` to 2.0.0, `esbuild` to 0.28.0, `@octokit/request` to 10.0.10, `undici` to 6.26.0, and `@actions/http-client` to 4.0.1.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Uses <a href="http://twitter.com/brunoborges">Bruno Borges'</a> brilliant
  * <a href="https://twitter.com/brunoborges/status/1565783443216416768?s=21&t=EoHl3kqopkK5pry-VVsSZA">hack</a> to extract a property
  * from a Maven `pom.xml` and export it to an environment variable
- * or property that other things, like the `setup-java` Github Action, can use.
+ * or property that other things, like the `setup-java` GitHub Action, can use.
  *
  * @author Josh Long
  */
@@ -14,7 +14,7 @@ const exec = require("@actions/exec");
 const fs = require('fs');
 
 /**
- * makes it a little more clean to exec something. make sure to redirect all other file descripters to stdout!
+ * makes it a little more clean to exec something. Make sure to redirect all other file descriptors to stdout!
  * @param {*} cmd
  * @param {*} args
  * @param {*} stdoutListener
@@ -68,7 +68,7 @@ try {
         if (gradleGroovy || gradleKotlin) {
             cmd("./gradlew", [ ':properties','--property','sourceCompatibility' ], outputBuffer => {
                 const buff = outputBuffer.toString();
-                if (buff.indexOf('sourceCompatibility') != -1) {
+                if (buff.indexOf('sourceCompatibility') !== -1) {
                     console.log('sourceCompatibility: ' + buff)
                 } else {
                     console.log('nope!')
